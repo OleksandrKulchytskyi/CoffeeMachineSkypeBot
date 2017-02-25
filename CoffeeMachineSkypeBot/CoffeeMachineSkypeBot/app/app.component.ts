1ae2ba834c92b66a09d7d31bdfb42438f00c157e
Oleksandr Kulchytskyi <sashaas@inbox.ru>
2017-02-25 19:53:21 +0200

    Injected Angular JS 2 libraries

---
+++ CoffeeMachineSkypeBot/CoffeeMachineSkypeBot/app/app.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from "@angular/core";
+
+@Component(
+	{
+		selector: 'my-app',
+		template: `<h1>Welcone {{name}}!</h1>
+					<label>Введите имя:</label>
+					<input [(ngModel)]="name" placeholder="name">`
+	}
+)
+
+export class AppComponent {
+	name = '';
+}
