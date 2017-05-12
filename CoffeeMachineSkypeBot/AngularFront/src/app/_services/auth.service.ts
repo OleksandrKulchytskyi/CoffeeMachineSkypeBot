f759cf4684e45426976e9a34ffa5ba7fdfd7407c
sashaas88 <sashaas@inbox.ru>
2017-05-12 10:59:58 -0400

    Fixed auth config via ConfigService

--- CoffeeMachineSkypeBot/AngularFront/src/app/_services/auth.service.ts
+++ CoffeeMachineSkypeBot/AngularFront/src/app/_services/auth.service.ts
@@ -3,13 +3,18 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { User } from '../_models/index';
+import { Config } from '../config';
 
 @Injectable()
 export class AuthService {
 
+	private  _configuration: Config;
 	storageItem = "currentUser";
 
-	constructor(private http: Http) { }
+	constructor(private http: Http,
+				private config:Config) { 
+		this._configuration = config;
+	}
 
 	isAuthenticated()
 	{
@@ -37,16 +42,24 @@ export class AuthService {
 		const body = JSON.stringify({ UserName: username, Password: password });
 		const headers = new Headers({ 'Content-Type': 'application/json' });
 		const options = new RequestOptions({ headers: headers });
+		
+		let apiPath = '/api/auth';
+		const data = this._configuration.getConfigData();
 
-		return this.http.post('/api/auth', body, options)
-			.map((response: Response) => {
-				// login successful if there's a jwt token in the response
-				const user = response.json();
-				if (user && user.token) {
-					// store user details and jwt token in local storage to keep user logged in between page refreshes
-					localStorage.setItem(this.storageItem, JSON.stringify(user));
-				}
-			});
+		if(data){
+			apiPath = data.apiHostPath + apiPath;
+			console.log(apiPath);
+		}
+
+		return this.http.post(apiPath, body, options)
+						.map((response: Response) => {
+							// login successful if there's a jwt token in the response
+							const user = response.json();
+							if (user && user.token) {
+								// store user details and jwt token in local storage to keep user logged in between page refreshes
+								localStorage.setItem(this.storageItem, JSON.stringify(user));
+							}
+						});
 	}
 
 	logout() {
