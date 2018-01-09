b9d53fd38b681338b0c8a331ffe21b86077f41b5
Oleksandr Kulchytskyi <Oleksandr.Kulchytskyi@infopulse.com>
2018-01-09 17:45:07 +0200

    Added aditional routes

--- CoffeeMachineSkypeBot/NgAppRefactored/src/app/app.module.ts
+++ CoffeeMachineSkypeBot/NgAppRefactored/src/app/app.module.ts
@@ -17,14 +17,16 @@ const environment = {
 import * as fromSerices from './services';
 import { Config } from './core/config';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './core/containers/page-not-found/page-not-found.component';
 
 // routes
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full'},
+  { path: 'page-not-found', component: PageNotFoundComponent },
   { path: 'login', loadChildren: '../login/login.module#LoginModule' },
   { path: 'users', loadChildren: '../users/users.module#UsersModule', canActivate: [fromSerices.AuthGuard] },
   // otherwise redirect to home
-	{ path: '**', redirectTo: '' }
+	{ path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
