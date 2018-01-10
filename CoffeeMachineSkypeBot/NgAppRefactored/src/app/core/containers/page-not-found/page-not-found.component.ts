14ea8b0ebf0860e7ec7a9912a3decca69b143abe
Oleksandr Kulchytskyi <Oleksandr.Kulchytskyi@infopulse.com>
2018-01-10 13:26:27 +0200

    Fixed isseu related to the styling

--- CoffeeMachineSkypeBot/NgAppRefactored/src/app/core/containers/page-not-found/page-not-found.component.ts
+++ CoffeeMachineSkypeBot/NgAppRefactored/src/app/core/containers/page-not-found/page-not-found.component.ts
@@ -1,14 +1,15 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Location } from '@angular/common';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'page-not-found',
   template: `
     <div class="pageNotFound">
     	<div class="pageNotFound-content">
-      	<h1>{{ 'PageNotFound.Title' | translate }}</h1>
-      	<h3>{{ 'PageNotFound.Subtitle' | translate }}</h3>
-      	<button (click)="goBack()">{{ 'PageNotFound.Button' | translate }}</button>
+      	<h1>{{ title }}</h1>
+      	<h3>{{ subTitle }}</h3>
+      	<button (click)="goBack()">{{ btnName }}</button>
     	</div>
     </div>
   `,
@@ -16,8 +17,19 @@ import { Location } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
+
+  public title: string;
+  public subTitle: string;
+  public btnName: string;
+
   constructor(private location: Location) {}
+
+  ngOnInit() {
+    this.title = 'Page was not found.';
+    this.subTitle = 'Pageyou are navigated on was not found.';
+    this.btnName = 'Go Back';
+  }
 
   public goBack() {
     this.location.back();
